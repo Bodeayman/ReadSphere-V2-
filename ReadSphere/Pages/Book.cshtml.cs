4af23f705d695906ff05a20c6705d1c53a4a58dc
Abdullah Ayman <abdulluhayman@gmail.com>
2024-12-01 23:26:11 +0200

    Some samples for the books and clubs , backend

--- ReadSphere/Pages/Book.cshtml.cs
+++ ReadSphere/Pages/Book.cshtml.cs
@@ -1,25 +1,78 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 using System.Collections.Generic;
+using System.Data;
 
 public class BookModel : PageModel
 {
     public List<Book> Books { get; set; }
-
-    public void OnGet()
-    {
-        // Mock data (Replace with data from your database)
-        Books = new List<Book>
-        {
-            new Book { Id = 1, Title = "The Alchemist", Author = "Ayman" },
-            new Book { Id = 2, Title = "1984", Author = "Dsoqi" },
-            new Book { Id = 3, Title = "To Kill a Mockingbird", Author = "somebody" }
-        };
-    }
+    public int count { get; set; }
 
     public class Book
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
+
+        public string Publisher { get; set; }
+        public string Language { get; set; }
+
+        public string cover_image { get; set; }
+
+        public string review_id { get; set; }
     }
+    public void OnGet()
+    {
+        Books = new List<Book>();
+
+        using (SqlConnection connection = new SqlConnection("Server=ENGABDULLAH;Database=ReadSphere;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;"))
+        {
+            // SQL query to insert a worker into the Workers table
+            string query = "select * from BOOK";
+
+            SqlCommand cmd = new(query, connection);
+            // Create a DataAdapter to fill the DataTable
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+
+            // Create a DataTable to hold the data
+            DataTable dataTable = new DataTable();
+
+            try
+            {
+                // Open the connection
+                connection.Open();
+
+                // Fill the DataTable with the data from the database
+                dataAdapter.Fill(dataTable);
+
+                // Process the data from the DataTable
+                foreach (DataRow row in dataTable.Rows)
+                {
+                    int Id = Convert.ToInt32(row["Book_Id"]);
+                    string title = row["Title"].ToString();
+                    string Author = row["Author_Name"].ToString();
+                    string publisher = row["Publisher"].ToString();
+                    string Language = row["Language"].ToString();
+                    string cover_image = row["Cover_Image"].ToString();
+                    string review_id = row["Review_Id"].ToString();
+                    Book book = new Book();
+                    book.Id = Id;
+                    book.Title = title;
+                    book.Author = Author;
+                    book.Publisher = publisher;
+                    book.Language = Language;
+                    book.cover_image = cover_image;
+                    book.review_id = review_id;
+                    Books.Add(book);
+                }
+                count = Books.Count;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+
+
 }
--- ReadSphere/Pages/Clubs.cshtml.cs
+++ ReadSphere/Pages/Clubs.cshtml.cs
@@ -1,15 +1,69 @@
+using System.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
 
 namespace ReadSphere.Pages
 {
     public class ClubsModel : PageModel
     {
-        public List<String> clubs = ["Zewail City Club", "ICPC Cooperation", "IEEE of ZC"];
+
+        public List<Club> Clubs { get; set; }
+        public int count { get; set; }
+
+        public class Club
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Desc { get; set; }
+
+
+        }
         public void OnGet()
         {
+            Clubs = new List<Club>();
+
+            using (SqlConnection connection = new SqlConnection("Server=ENGABDULLAH;Database=ReadSphere;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;"))
+            {
+                // SQL query to insert a worker into the Workers table
+                string query = "select * from club";
+
+                SqlCommand cmd = new(query, connection);
+                // Create a DataAdapter to fill the DataTable
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection);
+
+                // Create a DataTable to hold the data
+                DataTable dataTable = new DataTable();
+
+                try
+                {
+                    // Open the connection
+                    connection.Open();
+
+                    // Fill the DataTable with the data from the database
+                    dataAdapter.Fill(dataTable);
+
+                    // Process the data from the DataTable
+                    foreach (DataRow row in dataTable.Rows)
+                    {
+                        int Id = Convert.ToInt32(row["club_id"]);
+                        string Name = row["club_name"].ToString();
+                        string Desc = row["club_description"].ToString();
 
 
+                        Club club = new Club();
+                        club.Id = Id;
+                        club.Name = Name;
+                        club.Desc = Desc;
+                        Clubs.Add(club);
+                    }
+                    count = Clubs.Count;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+            }
         }
     }
 }
