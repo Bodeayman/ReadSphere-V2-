a65a84456e6116a67f72bb52734bf279f41e3aec
Dsoqi <202300894@zewailcity.edu.eg>
2024-12-02 21:14:02 +0200

    Ado.net Addbooks

--- ReadSphere/Pages/AddBook.cshtml.cs
+++ ReadSphere/Pages/AddBook.cshtml.cs
@@ -1,33 +1,60 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System;
 
 public class AddBookModel : PageModel
 {
-    private readonly DataContext _context;
-
-    public AddBookModel(DataContext context)
-    {
-        _context = context;
-    }
-
     [BindProperty]
     public Book Book { get; set; }
 
-    public void OnGet()
+    public class Book
     {
+        public int Id { get; set; } // can be generated in DB
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public string Language { get; set; }
+        public string cover_image { get; set; }
+        public string review_id { get; set; }
     }
 
-    public async Task<IActionResult> OnPostAsync()
+    public IActionResult OnPost()
     {
         if (!ModelState.IsValid)
         {
             return Page();
         }
+        string connectionString = "Server=ENGABDULLAH;Database=ReadSphere;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;";
 
-        _context.Books.Add(Book);
-        await _context.SaveChangesAsync();
+        // SQL query to insert a new book into the BOOK table
+        string query = "INSERT INTO BOOK (Title, Author_Name, Publisher, Language, Cover_Image, Review_Id) " +
+                       "VALUES (@Title, @Author, @Publisher, @Language, @CoverImage, @ReviewId)";
 
-        return RedirectToPage("/Index");
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            SqlCommand cmd = new SqlCommand(query, connection);
+
+            // parameters to avoid SQL injection
+            cmd.Parameters.AddWithValue("@Title", Book.Title);
+            cmd.Parameters.AddWithValue("@Author", Book.Author);
+            cmd.Parameters.AddWithValue("@Publisher", Book.Publisher);
+            cmd.Parameters.AddWithValue("@Language", Book.Language);
+            cmd.Parameters.AddWithValue("@CoverImage", Book.cover_image);
+            cmd.Parameters.AddWithValue("@ReviewId", Book.review_id);
+
+            try
+            {
+                connection.Open();
+                cmd.ExecuteNonQuery(); 
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+                return Page();
+            }
+        }
+
+        return RedirectToPage("/Books");// redirection to books page
     }
 }
